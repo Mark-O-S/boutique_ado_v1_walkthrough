9217b6c7837126fb8fb8b2fb8c9e27e47fd61aed
Mark-O-S <>
2023-08-12 22:47:29 +0000

    Add webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -7,10 +7,13 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
-import env
+import os
+if os.path.exists("env.py"):
+    import env
 
 import stripe
 import json
+
 
 @require_POST
 def cache_checkout_data(request):
@@ -49,7 +52,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
